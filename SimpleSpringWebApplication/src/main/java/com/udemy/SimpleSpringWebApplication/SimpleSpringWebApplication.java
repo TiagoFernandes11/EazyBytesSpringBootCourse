a1d3965d1569c7c8d2239393b763777b7ba15244
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-17 10:34:23 -0300

    Spring Initiazr

---
+++ SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/SimpleSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.SimpleSpringWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringWebApplication.class, args);
+	}
+
+}
---
+++ SimpleSpringWebApplication/src/test/java/com/udemy/SimpleSpringWebApplication/SimpleSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.SimpleSpringWebApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
