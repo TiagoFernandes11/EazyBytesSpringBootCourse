c874a2d3ccb7fc640043892e8410fcc2db2328a5
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-18 19:45:48 -0300

    Validation

--- SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/controller/ContactController.java
+++ SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/controller/ContactController.java
@@ -2,11 +2,14 @@ package com.udemy.SimpleSpringWebApplication.controller;
 
 import com.udemy.SimpleSpringWebApplication.model.Contact;
 import com.udemy.SimpleSpringWebApplication.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
@@ -22,14 +25,20 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
         }
 
         @RequestMapping("/contact")
-        public String displayContactPage() {
+        public String displayContactPage(Model model) {
+            model.addAttribute("contact", new Contact());
             return "contact.html";
         }
 
         @RequestMapping(value = "/saveMsg",method = POST)
-        public ModelAndView saveMessage(Contact contact){
+        public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors){
+
+            if(errors.hasErrors()){
+                log.error("Contact form validation failed due to : " + errors.toString());
+                return "contact.html";
+            }
             contactService.saveMessageDetails(contact);
-            return new ModelAndView("redirect:/contact");
+            return "redirect:/contact";
         }
 
     }
