b747246d42435f43f910659a5559c433bd1fb9cc
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-19 20:18:37 -0300

    InMemoryUserDetailsManager implemented

--- SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/config/ProjectSecurityConfig.java
+++ SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/config/ProjectSecurityConfig.java
@@ -3,6 +3,11 @@ package com.udemy.SimpleSpringWebApplication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -22,5 +27,21 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
