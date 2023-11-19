3493349a72b7d539ed788f7d2dbaa5a40dcda786
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2023-11-19 20:50:30 -0300

    adding templates

--- SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/config/ProjectSecurityConfig.java
+++ SimpleSpringWebApplication/src/main/java/com/udemy/SimpleSpringWebApplication/config/ProjectSecurityConfig.java
@@ -2,6 +2,7 @@ package com.udemy.SimpleSpringWebApplication.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,15 +17,20 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) ->
-                         requests.requestMatchers("", "/", "/home").permitAll()
+        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("", "/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
-                        .requestMatchers("/assets/**").permitAll());
-        http.formLogin(withDefaults());
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/assets/**").permitAll())
+                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                        .invalidateHttpSession(true).permitAll())
+                .httpBasic(Customizer.withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
     }
